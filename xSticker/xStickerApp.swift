747ab877465dfc6e24c7b339a64f6711f056f71e
W-Mai <1341398182@qq.com>
2021-10-14 16:00:39 +0800

    Initial Commit

---
+++ xSticker/xStickerApp.swift
@@ -0,0 +1,20 @@
+//
+//  xStickerApp.swift
+//  xSticker
+//
+//  Created by W-Mai on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct xStickerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
