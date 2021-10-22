00059b3983f6a367f7a891ad07d63a577c2ab7c0
W-Mai <1341398182@qq.com>
2021-10-22 14:31:10 +0800

    增加键盘拓展

---
+++ xStickerBoard/KeyboardViewController.swift
@@ -0,0 +1,71 @@
+//
+//  KeyboardViewController.swift
+//  xStickerBoard
+//
+//  Created by W-Mai on 2021/10/22.
+//
+
+import UIKit
+import CoreData
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    let persistence = PersistenceController.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.nextKeyboardButton.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        self.nextKeyboardButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        let context = persistence.container.viewContext
+        let req = Collections.fetchRequest() as NSFetchRequest<Collections>
+        let count = (try? context.count(for: req)) ?? 0
+        
+        self.nextKeyboardButton.setTitle("\(count)", for: [])
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
